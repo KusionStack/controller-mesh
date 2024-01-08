702a3dcf7547ce36919a5ef27078a6d5f73318ce
Chaer <qq978874169@gmail.com>
2024-01-08 17:59:51 +0800

    init runnable

--- sample/cmd/runner/runner.go
+++ sample/cmd/runner/runner.go
@@ -15,3 +15,84 @@
 */
 
 package runner
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	controlLabel = "sample.kusionstack.io/control-by"
+)
+
+var (
+	PodName = os.Getenv("POD_NAME")
+)
+
+func New(c client.Client) Runner {
+	return &runner{Client: c}
+}
+
+type Runner interface {
+	Start(ctx context.Context) error
+}
+
+type runner struct {
+	client.Client
+}
+
+// +kubebuilder:rbac:groups=,resources=namespaces,verbs=get;list;watch;create;update;patch
+
+func (r *runner) Start(ctx context.Context) error {
+	for {
+		if err := r.holdTestResources(ctx); err != nil {
+			return err
+		}
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context done")
+		case <-time.After(10 * time.Second):
+		}
+	}
+}
+
+func (r *runner) holdTestResources(ctx context.Context) error {
+	nss := &v1.NamespaceList{}
+	if err := r.List(ctx, nss); err != nil {
+		return err
+	}
+	var holdNs []string
+	for i := range nss.Items {
+		ns := &nss.Items[i]
+		holdNs = append(holdNs, ns.Name)
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			if err := r.Get(ctx, types.NamespacedName{Name: nss.Items[i].Name}, ns); err != nil {
+				if errors.IsNotFound(err) {
+					return nil
+				}
+				return err
+			}
+			if ns.Labels == nil {
+				ns.Labels = map[string]string{}
+			}
+			if controlName, ok := ns.Labels[controlLabel]; !ok || PodName != controlName {
+				ns.Labels[controlLabel] = PodName
+				return r.Update(ctx, ns)
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+	}
+	klog.Infof("hold namespaces %v", holdNs)
+	return nil
+}
